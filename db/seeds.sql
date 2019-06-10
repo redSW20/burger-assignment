30c5de11bb75d410dafa85c9197db257bec6f792
RedSW20 <kevbailey1227@gmail.com>
2019-06-10 15:48:13 -0500

    sql files written and ORM ready to setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale with Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Kahuna Matata Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Intimidator')
